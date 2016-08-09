c9f9138a2c9c7f3d746920c584165bbbc03f8d73
danielcodin <daniel@danielcoding.com>
2016-08-10 01:33:10 +0800

    Initial Commit

---
+++ PokeIV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeIV
+//
+//  Created by Daniel Tseng on 8/10/16.
+//  Copyright © 2016 Daniel Tseng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
